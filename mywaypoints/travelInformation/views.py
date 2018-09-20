d89bcecb8617a99edee6cfc7a10697a9b1d841a5
Muthu2093 <muthuvel@buffalo.edu>
2018-09-19 21:30:52 -0400

    Set up Main Project
    Set up Main Project

--- mywaypoints/travelInformation/views.py
+++ mywaypoints/travelInformation/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
-
-
+# Create your views here.
 def index(request):
-	return HttpResponse(html)
-    	
+    return HttpResponse("This is the main App")
